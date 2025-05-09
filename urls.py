866a923b3e84a6aad12715e71edc510d952e1311
YoussefElreweny <141136284+YoussefElreweny@users.noreply.github.com>
2025-05-09 21:21:16 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),
+    path('', views.task_list, name='task_list'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
---
+++ views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tasks/signup.html', {'form': form})
